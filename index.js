85f77db8699f9bd0b2b864fdea64a75b97bb11bd
Mateusz Krawczynski <matikrawczpl@gmail.com>
2025-06-02 22:49:26 +0100

    first proof of concept working
    Currently the websocket is hard coded with a port, there is some
    functionality to expose it as a setting in tosu

--- index.js
+++ index.js
@@ -2,65 +2,76 @@
 import WebSocketManager from './js/socket.js';
 const socket = new WebSocketManager('127.0.0.1:24050');
 
+// TODO: Allow configuring port later
+const veadoSocket = new WebSocket(`ws://127.0.0.1:8085/?n=blemtuber`);
+
+veadoSocket.onopen = () => {
+  console.log('Connected to VeadoTube WebSocket');
+}
+
 
 // cache values here to prevent constant updating
 const cache = {
-  h100: -1,
-  h50: -1,
-  h0: -1,
-  accuracy: -1,
+  maxCombo: 0,
+  combo: -1,
 };
 
 
+function bubanMoment(combo_percent) {
+  if (combo_percent < 0.33) {
+    return;
+  }
+  let bubanTime = 10**((3 * combo_percent - 1) / 2) * 1000;
 
-// Smoouth numbers update
-const accuracy = new CountUp('accuracy', 0, 0, 2, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
-const h100 = new CountUp('h100', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
-const h50 = new CountUp('h50', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
-const h0 = new CountUp('h0', 0, 0, 0, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." })
+  // Send buban payload
+  const bubanPayload = {
+    "event": "payload",
+    "type": "stateEvents",
+    "id": "mini",
+    "payload": {
+        "event": "set",
+        "state": "BUBAN"
+    }
+  }
+  veadoSocket.send("nodes: " + JSON.stringify(bubanPayload));
+
+  // Restore BLEM state after bubanTime
+  setTimeout(() => {
+    restoreBlem();
+  }, bubanTime);
+  console.log(`BUBAN mode activated for ${bubanTime / 1000} seconds`);
+}
 
 
-
+function restoreBlem() {
+  const blemPayload = {
+    "event": "payload",
+    "type": "stateEvents",
+    "id": "mini",
+    "payload": {
+        "event": "set",
+        "state": "BLEM"
+    }
+  }
+  veadoSocket.send("nodes: " + JSON.stringify(blemPayload));
+}
 
 // receive message update from websocket
-socket.api_v2(({ play }) => {
+socket.api_v2(({ play, beatmap }) => {
   try {
-    // check if value has changed
-    if (cache.h100 !== play.hits['100']) {
-      // update cache
-      cache.h100 = play.hits['100'];
+    if (cache.combo !== play.combo) {
+      if (play.combo.current < cache.combo.current) {
+        let brokenComboRatio = cache.combo.current / cache.maxCombo;
+        console.log(`Broken combo percent: ${brokenComboRatio * 100}%`);
+        bubanMoment(brokenComboRatio);
+      }
 
+      cache.combo = play.combo;
+    }
 
-      //      IMPORTANT   !!   USE ONE OF THEM
-
-      // update html via countup
-      h100.update(play.hits['100']);
-
-      // update html via js
-      document.getElementById('h100').innerHTML = play.hits['100'];
-    };
-
-
-
-    if (cache.h50 !== play.hits['50']) {
-      cache.h50 = play.hits['50'];
-      h50.update(play.hits['50']);
-    };
-
-    if (cache.h0 !== play.hits['0']) {
-      cache.h0 = play.hits['0'];
-      h0.update(play.hits['0']);
-    };
-
-    if (cache.accuracy != play.accuracy) {
-      cache.accuracy = play.accuracy;
-      accuracy.update(play.accuracy);
-    };
-
-    if (cache.pp !== Math.round(play.pp.current)) {
-      cache.pp = Math.round(play.pp.current);
-      document.getElementById('pp').innerHTML = Math.round(play.pp.current);
-    };
+    if (cache.maxCombo != beatmap.maxCombo) {
+      cache.maxCombo = beatmap.stats.maxCombo;
+    }
   } catch (error) {
     console.log(error);
   };
