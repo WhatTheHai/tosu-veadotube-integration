78c0a072433369f83507dddb4cfdf8a82a074c26
WhatTheHai <haiboy@hotmail.nl>
2025-06-03 01:28:52 +0200

    Small refactor and settings concept

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// connecting to websocket
+// Connecting to websocket
 import WebSocketManager from './js/socket.js';
 const socket = new WebSocketManager('127.0.0.1:24050');
 
-// TODO: Allow configuring port later
+// TODO: Allow configuring port later, default is 8085
 const veadoSocket = new WebSocket(`ws://127.0.0.1:8085/?n=blemtuber`);
 
 veadoSocket.onopen = () => {
@@ -10,10 +10,11 @@ veadoSocket.onopen = () => {
 }
 
 
-// cache values here to prevent constant updating
+// Cache values here to prevent constant updating
 const cache = {
   maxCombo: 0,
   combo: -1,
+  BubanMinimum: 1
 };
 
 
@@ -24,15 +25,7 @@ function bubanMoment(combo_percent) {
   let bubanTime = 10**((3 * combo_percent - 1) / 2) * 1000;
 
   // Send buban payload
-  const bubanPayload = {
-    "event": "payload",
-    "type": "stateEvents",
-    "id": "mini",
-    "payload": {
-        "event": "set",
-        "state": "BUBAN"
-    }
-  }
+  const bubanPayload = createSetPayLoad("BUBAN");
   veadoSocket.send("nodes: " + JSON.stringify(bubanPayload));
 
   // Restore BLEM state after bubanTime
@@ -44,21 +37,40 @@ function bubanMoment(combo_percent) {
 
 
 function restoreBlem() {
-  const blemPayload = {
+  const blemPayload = createSetPayLoad("BLEM");
+  veadoSocket.send("nodes: " + JSON.stringify(blemPayload));
+}
+
+// Payload blueprint for VeadoTube for changing state
+function createSetPayLoad(state) {
+  return {
     "event": "payload",
     "type": "stateEvents",
     "id": "mini",
     "payload": {
         "event": "set",
-        "state": "BLEM"
+        "state": state
     }
   }
-  veadoSocket.send("nodes: " + JSON.stringify(blemPayload));
 }
+
+socket.sendCommand('getSettings', encodeURI(window.COUNTER_PATH));
+socket.commands(async (data) => {
+    try {
+        const { command, message } = data;
+        if (command != 'getSettings') return;
+        if (message['BubanMinimum'] != null) {
+            cache['BubanMinimum'] = message['BubanMinimum'];
+        }
+    } catch (error) {
+        console.log(error);
+    };
+});
 
 // receive message update from websocket
 socket.api_v2(({ play, beatmap }) => {
   try {
+    console.log(cache.BubanMinimum);
     if (cache.combo !== play.combo) {
       if (play.combo.current < cache.combo.current) {
         let brokenComboRatio = cache.combo.current / cache.maxCombo;
