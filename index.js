3168006915dd9e48575be4ba0c289644b8fd8388
Daniel <dfw056@student.bham.ac.uk>
2025-06-03 14:24:02 +0100

    added length scaling and reverse choke bubanMoment, fixed buban appearing on map switch

--- index.js
+++ index.js
@@ -14,18 +14,22 @@ veadoSocket.onopen = () => {
 const cache = {
   maxCombo: 0,
   combo: -1,
-  BubanMinimum: 1
+  baseBuban: 1000,
+  mapLength: -1,
+  reverseChoked: 0
 };
 
 
 function bubanMoment(combo_percent) {
+  //TODO change percent threshold with a setting
   if (combo_percent < 0.33) {
     return;
   }
-  let bubanTime = 10**((3 * combo_percent - 1) / 2) * 1000;
-  if (cache.BubanMinimum > bubanTime) {
-    bubanTime = cache.BubanMinimum;
-  }
+  let bubanTime = 10**((3 * combo_percent - 1) / 2) * cache.baseBuban;
+  
+  //scale bubanMoment by map length
+  //TODO add setting for preferred map length, currently 3 minutes
+  bubanTime = bubanTime * cache.mapLength/180000;
 
   // Send buban payload
   const bubanPayload = createSetPayLoad("BUBAN");
@@ -62,8 +66,8 @@ socket.commands(async (data) => {
     try {
         const { command, message } = data;
         if (command != 'getSettings') return;
-        if (message['BubanMinimum'] != null) {
-            cache['BubanMinimum'] = message['BubanMinimum'];
+        if (message['baseBuban'] != null) {
+            cache['baseBuban'] = message['baseBuban'];
         }
     } catch (error) {
         console.log(error);
@@ -73,19 +77,34 @@ socket.commands(async (data) => {
 // receive message update from websocket
 socket.api_v2(({ play, beatmap }) => {
   try {
-    console.log(cache.BubanMinimum);
     if (cache.combo !== play.combo) {
       if (play.combo.current < cache.combo.current) {
-        let brokenComboRatio = cache.combo.current / cache.maxCombo;
-        console.log(`Broken combo percent: ${brokenComboRatio * 100}%`);
-        bubanMoment(brokenComboRatio);
-      }
+		console.log(`${play.hits[0]} && ${play.hits.sliderBreaks}`);
+		if (play.hits[0] || play.hits.sliderBreaks) {
+		  console.log(`inside choke statement`);
+		  let brokenComboRatio = cache.combo.current / cache.maxCombo;
+		  console.log(`Broken combo percent: ${brokenComboRatio * 100}%`);
+		  bubanMoment(brokenComboRatio);
+		}
+	  }
 
       cache.combo = play.combo;
+	  if (beatmap.time.live >= cache.mapLength) {
+		let reverseChokeRatio = cache.combo.current / cache.maxCombo;
+		if (reverseChokeRatio > 0.85 && !cache.reverseChoked) {
+			if (play.hits[0] || play.hits.sliderBreaks) {
+				console.log(`inside reverse choke statement`);
+				bubanMoment(reverseChokeRatio)
+				cache.reverseChoked = 1;
+			}
+		}
+	  }
     }
 
-    if (cache.maxCombo != beatmap.maxCombo) {
+    if (cache.maxCombo != beatmap.stats.maxCombo) {
       cache.maxCombo = beatmap.stats.maxCombo;
+	  cache.mapLength = beatmap.time.lastObject - beatmap.time.firstObject;
+	  cache.reverseChoked = 0;
     }
   } catch (error) {
     console.log(error);
