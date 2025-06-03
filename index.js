82c4d269d1ee4b86fb6bd57e6721d36061a22934
WhatTheHai <haiboy@hotmail.nl>
2025-06-03 16:38:15 +0200

    Added port and minimum combo percentage configuration

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 // Connecting to websocket
 import WebSocketManager from './js/socket.js';
 const socket = new WebSocketManager('127.0.0.1:24050');
-
-// TODO: Allow configuring port later, default is 8085
-const veadoSocket = new WebSocket(`ws://127.0.0.1:8085/?n=blemtuber`);
-
-veadoSocket.onopen = () => {
-  console.log('Connected to VeadoTube WebSocket');
-}
+socket.onopen = onSocketOpen();
+let veadoSocket;
+let veadoReconnectTimer;
 
 
 // Cache values here to prevent constant updating
@@ -16,13 +12,44 @@ const cache = {
   combo: -1,
   baseBuban: 1000,
   mapLength: -1,
-  reverseChoked: 0
+  reverseChoked: 0,
+  veadoPort: 8085,
+  minimumComboPercent: 33
 };
 
 
+function connectVeadoSocket() {
+  // When changing the port, close the previous connection if it exists
+  if (veadoSocket && veadoSocket.readyState === WebSocket.OPEN) {
+    veadoSocket.close();
+  }
+  
+  clearTimeout(veadoReconnectTimer);
+
+  // Connect to VeadoTube WebSocket
+  veadoSocket = new WebSocket(`ws://127.0.0.1:${cache.veadoPort}/?n=blemtuber`);
+
+  veadoSocket.onopen = () => {
+    console.log('Connected to VeadoTube WebSocket');
+  }
+  veadoSocket.onerror = (error) => {
+    console.error('WebSocket error:', error);
+  }
+  veadoSocket.onclose = () => {
+    console.log('VeadoTube WebSocket connection closed, attempting to reconnect...');
+    // Attempt to reconnect after a three second delay.
+    veadoReconnectTimer = setTimeout(connectVeadoSocket, 3000);
+  }
+}
+
+
+// veadoSocket.onopen = () => {
+//   console.log('Connected to VeadoTube WebSocket');
+// }
+
 function bubanMoment(combo_percent) {
   //TODO change percent threshold with a setting
-  if (combo_percent < 0.33) {
+  if (combo_percent < (minimumComboPercent/100)) {
     return;
   }
   let bubanTime = 10**((3 * combo_percent - 1) / 2) * cache.baseBuban;
@@ -61,18 +88,27 @@ function createSetPayLoad(state) {
   }
 }
 
-socket.sendCommand('getSettings', encodeURI(window.COUNTER_PATH));
-socket.commands(async (data) => {
-    try {
-        const { command, message } = data;
-        if (command != 'getSettings') return;
-        if (message['baseBuban'] != null) {
-            cache['baseBuban'] = message['baseBuban'];
-        }
-    } catch (error) {
-        console.log(error);
-    };
-});
+function onSocketOpen() {
+  socket.sendCommand('getSettings', encodeURI(window.COUNTER_PATH));
+  socket.commands(async (data) => {
+      try {
+          const { command, message } = data;
+          if (command != 'getSettings') return;
+          if (message['baseBuban'] != null) {
+              cache['baseBuban'] = message['baseBuban'];
+          }
+          if (message['veadoPort'] != null && message['veadoPort'] != cache['veadoPort']) {
+              cache['veadoPort'] = message['veadoPort'];
+              connectVeadoSocket();
+          }
+          if (message['minimumComboPercent'] != null) {
+              cache['minimumComboPercent'] = message['minimumComboPercent'];
+          }
+      } catch (error) {
+          console.log(error);
+      };
+  });
+}
 
 // receive message update from websocket
 socket.api_v2(({ play, beatmap }) => {
